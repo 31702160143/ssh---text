a467b6923eee0a997a46ad6271814a5e9980e97d
10.12 <a10.12@MacBook-Pro.local>
2020-02-20 18:13:33 +0800

    创建了cat类

---
+++ github的SSH的测试/github的SSH的测试/XWcat.h
@@ -0,0 +1,13 @@
+//
+//  XWcat.h
+//  github的SSH的测试
+//
+//  Created by 10.12 on 2020/2/20.
+//  Copyright © 2020年 学文. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XWcat : NSObject
+
+@end
---
+++ github的SSH的测试/github的SSH的测试/XWcat.m
@@ -0,0 +1,13 @@
+//
+//  XWcat.m
+//  github的SSH的测试
+//
+//  Created by 10.12 on 2020/2/20.
+//  Copyright © 2020年 学文. All rights reserved.
+//
+
+#import "XWcat.h"
+
+@implementation XWcat
+
+@end
